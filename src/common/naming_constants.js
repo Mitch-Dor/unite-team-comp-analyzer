e98c09cda7cb1e0c4679e9d0b668f4b1a5468522
Mitchell Dorward <mitchdor48@gmail.com>
2024-05-21 22:08:31 -0400

    Set it up to be a react app

---
+++ src/common/naming_constants.js
@@ -0,0 +1,91 @@
+// All the basic naming conventions of things. Keeping as const cause naming conventions may change. For example: Alolan Ninetales could be A-Ninetales, Alolan_Ninetales, etc
+// Also just makes it easier to prevent typos and just change stuff as needed.
+
+// Pokemon
+export const CINDERACE_NAME = 'Cinderace';
+export const GARCHOMP_NAME = 'Garchomp';
+export const GENGAR_NAME = 'Gengar';
+export const CRAMORANT_NAME = 'Cramorant';
+export const ALOLANNINETALES_NAME = 'Alolan-Ninetales';
+export const WIGGLYTUFF_NAME = 'Wigglytuff';
+export const MACHAMP_NAME = 'Machamp';
+export const ABSOL_NAME = 'Absol';
+export const SLOWBRO_NAME = 'Slowbro';
+export const MRMIME_NAME = 'Mr-Mime';
+export const VENUSAUR_NAME = 'Venusaur';
+export const LUCARIO_NAME = 'Lucario';
+export const TALONFLAME_NAME = 'Talonflame';
+export const ELDEGOSS_NAME = 'Eldegoss';
+export const GRENINJA_NAME = 'Greninja';
+export const CRUSTLE_NAME = 'Crustle';
+export const SNORLAX_NAME = 'Snorlax';
+export const CHARIZARD_NAME = 'Charizard';
+export const PIKACHU_NAME = 'Pikachu';
+export const ZERAORA_NAME = 'Zeraora';
+export const GARDEVOIR_NAME = 'Gardevoir';
+export const BLISSEY_NAME = 'Blissey';
+export const BLASTOISE_NAME = 'Blastoise';
+export const MAMOSWINE_NAME = 'Mamoswine';
+export const SYLVEON_NAME = 'Sylveon';
+export const GREEDENT_NAME = 'Greedent';
+export const DECIDUEYE_NAME = 'Decidueye';
+export const TSAREENA_NAME = 'Tsareena';
+export const DRAGONITE_NAME = 'Dragonite';
+export const TREVENANT_NAME = 'Trevenant';
+export const AEGISLASH_NAME = 'Aegislash';
+export const HOOPA_NAME = 'Hoopa';
+export const DURALUDON_NAME = 'Duraludon';
+export const AZUMARILL_NAME = 'Azumarill';
+export const ESPEON_NAME = 'Espeon';
+export const DELPHOX_NAME = 'Delphox';
+export const GLACEON_NAME = 'Glaceon';
+export const BUZZWOLE_NAME = 'Buzzwole';
+export const TYRANITAR_NAME = 'Tyranitar';
+export const MEW_NAME = 'Mew';
+export const DODRIO_NAME = 'Dodrio';
+export const SCIZOR_NAME = 'Scizor';
+export const SCYTHER_NAME = 'Scyther';
+export const CLEFABLE_NAME = 'Clefable';
+export const ZOROARK_NAME = 'Zoroark';
+export const SABLEYE_NAME = 'Sableye';
+export const URSHIFUSS_NAME = 'Urshifu-SS'; // Single-Strike (Red)
+export const URSHIFURS_NAME = 'Urshifu-RS'; // Rapid-Strike (Blue)
+export const DRAGAPULT_NAME = 'Dragapult';
+export const COMFEY_NAME = 'Comfey';
+export const ZACIAN_NAME = 'Zacian';
+export const GOODRA_NAME = 'Goodra';
+export const LAPRAS_NAME = 'Lapras';
+export const CHANDELURE_NAME = 'Chandelure';
+export const UMBREON_NAME = 'Umbreon';
+export const LEAFEON_NAME = 'Leafeon';
+export const INTELEON_NAME = 'Inteleon';
+export const MEWTWOY_NAME = 'Mewtwo-Y';
+export const MEWTWOX_NAME = 'Mewtwo-X';
+export const BLAZIKEN_NAME = 'Blaziken';
+export const MIMIKYU_NAME = 'Mimikyu';
+export const MEOWSCARADA_NAME = 'Meowscarada';
+export const METAGROSS_NAME = 'Metagross';
+export const GYARADOS_NAME = 'Gyarados';
+export const MIRAIDON_NAME = 'Miraidon';
+export const FALINKS_NAME = 'Falinks';
+
+
+// Roles
+export const ATTACKER = 'Attacker';
+export const DEFENDER = 'Defender';
+export const ALL_ROUNDER = 'All-Rounder';
+export const SPEEDSTER = 'Speedster';
+export const SUPPORTER = 'Supporter';
+
+//// Attributes
+// General Attributes
+export const GOOD_LANE = 'Good Laner';
+export const BAD_LANE = 'Bad Laner';
+export const GOOD_JUNGLER = 'Good Jungler';
+export const BAD_JUNGLER = 'Bad Jungler';
+export const STRONG_EARLY = 'Strong Early Game';
+export const STRONG_MIDDLE = 'Strong Mid Game';
+export const STRONG_LATE = 'Strong Late Game';
+export const WEAK_EARLY = 'Weak Early Game';
+export const WEAK_MIDDLE = 'Weak Mid Game';
+export const WEAK_LATE = 'Weak Late Game';
