23c56e95a19bc8e96d0b49fe0272fdecb5ef234f
Mitchell_Dorward <mgd225@lehigh.edu>
2025-04-04 23:30:16 -0400

    Made it so when it's an AI pick the user cannot target a pokemon

--- frontend/src/components/mainComponents/js/draftSupport/DraftListing.jsx
+++ frontend/src/components/mainComponents/js/draftSupport/DraftListing.jsx
@@ -4,14 +4,28 @@ const routes = require('../../../../common/route_constants.js');
 
 const DraftListing = ({ pokemonList, team1Bans, team2Bans, team1Picks, team2Picks, draftState, updateDraftState, updatePokemonStatus, draftProgression, numUsers, settings, targetPokemon, setTargetPokemon }) => { // Adding {} around this destructures the props. Otherwise everything will just be in one props obejct
 
+    function targetPokemon(pokemon){
+        // Check if the pokemon is already picked/banned
+        const isUnavailable = team1Bans.includes(pokemon) || team2Bans.includes(pokemon) || team1Picks.includes(pokemon) || team2Picks.includes(pokemon);
+        if (draftState !== 'done' && !isUnavailable){
+            if(numUsers == 2) {
+                // It's definitely a user turn
+                setTargetPokemon(pokemon);
+            } else if (numUsers == 1) {
+                if ((settings.userTurn === "first" && draftState.includes("team1")) || (settings.userTurn === "second" && draftState.includes("team2"))){
+                    // There is an AI but it's the user's turn
+                    setTargetPokemon(pokemon);
+                }
+            }
+        }
+    }
+
     return (
         <>
             {pokemonList && pokemonList.length > 0 ? (
                 pokemonList.map(pokemon => {
-                    // Check if the pokemon is already picked/banned
-                    const isUnavailable = team1Bans.includes(pokemon) || team2Bans.includes(pokemon) || team1Picks.includes(pokemon) || team2Picks.includes(pokemon);
                     return (
-                        <div className={`draftCharacter ${pokemon.pokemon_class} ${isUnavailable ? 'unavailable' : 'available'} ${targetPokemon === pokemon ? 'targeted' : ''}`} key={pokemon.pokemon_id} onClick={() => {if(!isUnavailable){setTargetPokemon(pokemon)}}}>
+                        <div className={`draftCharacter ${pokemon.pokemon_class} ${isUnavailable ? 'unavailable' : 'available'} ${targetPokemon === pokemon ? 'targeted' : ''}`} key={pokemon.pokemon_id} onClick={() => {targetPokemon(pokemon)}}>
                             <img className="characterPortrait" src={`/assets/Draft/headshots/${pokemon.pokemon_name}.png`} alt={pokemon.pokemon_name} />
                             <h4>{pokemon.pokemon_name}</h4>
                         </div>
