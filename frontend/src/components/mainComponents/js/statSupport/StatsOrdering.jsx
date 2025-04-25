1b5e9ffb3c8c2478fc0e3acace564455a052987c
Mitchell_Dorward <mgd225@lehigh.edu>
2025-04-24 23:44:55 -0400

    Added a quick info hover to order by

--- frontend/src/components/mainComponents/js/statSupport/StatsOrdering.jsx
+++ frontend/src/components/mainComponents/js/statSupport/StatsOrdering.jsx
@@ -5,6 +5,19 @@ function StatsOrdering({ setOrderBy }) {
     return (
         <div id="orderingContainer">
             <h3>Order By</h3>
+            <div id="orderInfo" title="
+            Filter descriptions:
+
+            Player: Cuts all picks to picks played specifically by that player. Bans are any bans made by the team the player was on.
+
+            Team: Cuts all picks to picks played by that team. Bans are any bans made by that team.
+
+            Region: Cuts all picks to picks made by teams belonging to that region. Bans are any bans made by a team belonging to that region.
+
+            Event: Cuts all picks and bans to picks/bans that happened at that particular event.
+
+            Date: Limits the search to events that happened within the specified time range.
+            ">?</div>
             <select name="orderBy" id="orderBy" onChange={(e) => setOrderBy(e.target.value)}>
                 <option value="all">All Stats</option>
                 <option value="ban">Ban Rate</option>
