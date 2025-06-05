2d55bfea782b758c8e7d5e3e94ee849b6e308e65
Mitchell_Dorward <mgd225@lehigh.edu>
2025-06-04 20:22:38 -0400

    Removing Unnecessary things

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';  // Import from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { UserProvider } from './context/UserContext';
 
 // Create a root for the application
@@ -16,8 +15,3 @@ root.render(
     </UserProvider>
   </BrowserRouter>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
