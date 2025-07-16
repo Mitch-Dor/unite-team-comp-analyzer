de2a000b7e11beb1e56b9f6f1e8cf15ec540faa9
Mitchell_Dorward <mgd225@lehigh.edu>
2025-07-16 13:14:04 -0400

    Added all the display visuals for new advanced data for comps

--- frontend/src/components/mainComponents/js/compSupport/CompDisplay.jsx
+++ frontend/src/components/mainComponents/js/compSupport/CompDisplay.jsx
@@ -1,6 +1,9 @@
 function CompDisplay({ match, teamData, teamNumber, hasAdvanced }) {
     const firstPickNumbers = [1, 4, 5, 8, 9];
     const secondPickNumbers = [2, 3, 6, 7, 10];
+    const totalDealt = teamData.pokemon_data[0][3] + teamData.pokemon_data[1][3] + teamData.pokemon_data[2][3] + teamData.pokemon_data[3][3] + teamData.pokemon_data[4][3];
+    const totalTaken = teamData.pokemon_data[0][4] + teamData.pokemon_data[1][4] + teamData.pokemon_data[2][4] + teamData.pokemon_data[3][4] + teamData.pokemon_data[4][4];
+    const totalHealed = teamData.pokemon_data[0][5] + teamData.pokemon_data[1][5] + teamData.pokemon_data[2][5] + teamData.pokemon_data[3][5] + teamData.pokemon_data[4][5];
 
     return (
         <div className={`team-side ${match.winningTeam === 1 && teamNumber === 1 ? 'winning-team' : match.winningTeam === 2 && teamNumber === 2 ? 'winning-team' : ''}`}>
@@ -29,8 +32,8 @@ function CompDisplay({ match, teamData, teamNumber, hasAdvanced }) {
             
             <div className="team-comp">
             {teamData.pokemon.map((pokemon, i) => (
-                <>
                 <div className="draft-row" key={i}>
+                <div className="base-data">
                     <div className="draft-row-number-headshot-container">
                         <div className="draft-number">{teamData.firstPick === true ? firstPickNumbers[i] : secondPickNumbers[i]}</div>
                         <img 
@@ -61,13 +64,22 @@ function CompDisplay({ match, teamData, teamNumber, hasAdvanced }) {
                         <div className="statNumber kills">{teamData.pokemon_data[i][0]}</div>
                         <div className="statNumber assists">{teamData.pokemon_data[i][1]}</div>
                         <div className="statNumber scored">{teamData.pokemon_data[i][2]}</div>
-                        <div className="statBar dealt">{teamData.pokemon_data[i][3]}</div>
-                        <div className="statBar taken">{teamData.pokemon_data[i][4]}</div>
-                        <div className="statBar healed">{teamData.pokemon_data[i][5]}</div>
+                        <div className="statBar dealt">
+                            <div className="statBarFill red" style={{ width: `${(teamData.pokemon_data[i][3] / totalDealt) * 100}%` }}></div>
+                            <span>{teamData.pokemon_data[i][3]} <br /> {((teamData.pokemon_data[i][3] / totalDealt) * 100).toFixed(1)}%</span>
+                        </div>
+                        <div className="statBar taken">
+                            <div className="statBarFill blue" style={{ width: `${(teamData.pokemon_data[i][4] / totalTaken) * 100}%` }}></div>
+                            <span>{teamData.pokemon_data[i][4]} <br /> {((teamData.pokemon_data[i][4] / totalTaken) * 100).toFixed(1)}%</span>
+                        </div>
+                        <div className="statBar healed">
+                            <div className="statBarFill green" style={{ width: `${(teamData.pokemon_data[i][5] / totalHealed) * 100}%` }}></div>
+                            <span>{teamData.pokemon_data[i][5]} <br /> {((teamData.pokemon_data[i][5] / totalHealed) * 100).toFixed(1)}%</span>
+                        </div>
                         <div className={`positionIndicator ${teamData.pokemon_data[i][6]}`}></div>
                     </div>
                 }
-                </>
+                </div>
             ))}
             </div>
         </div>
