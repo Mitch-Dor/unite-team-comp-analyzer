bb095a9ac2836c7ec906a5c3423b4c92510fc9fa
Mitchell_Dorward <mgd225@lehigh.edu>
2025-08-23 21:41:17 -0400

    OAuth improvement

--- backend/database/auth.js
+++ backend/database/auth.js
@@ -18,18 +18,12 @@ class Auth {
                         resolve(res.rows[0]);
                     } else {
                         // User does not exist, add them to the database and sign in
-                        this.db.query('INSERT INTO users (user_google_id, user_name, user_email) VALUES ($1, $2, $3)', [googleId, name, email], function(err) {
+                        this.db.query('INSERT INTO users (user_google_id, user_name, user_email) VALUES ($1, $2, $3) RETURNING *', [googleId, name, email], function(err, res) {
                             if (err) {
                                 reject(err);
                             } else {
                                 // After inserting, get the user to return it
-                                this.db.query('SELECT * FROM users WHERE user_google_id = $1', [googleId], (err, res) => {
-                                    if (err) {
-                                        reject(err);
-                                    } else {
-                                        resolve(res.rows[0]);
-                                    }
-                                });
+                                resolve(res.rows[0]);
                             }
                         });
                     }
